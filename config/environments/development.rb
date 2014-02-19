cb197634eee783a572d9f2a35b2b4ead87c74699
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-19 11:29:51 -0500

    fixed environmental variables for email function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,16 @@ OldNews::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+# config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'fromoldnewsapp',
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true  }
 end
+
