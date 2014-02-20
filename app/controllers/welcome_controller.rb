d7da2dc8ba8677c17dc707285eb7b1b403d9241b
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-20 17:07:55 -0500

    refactored article search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 # welcome_controller.rb
 class WelcomeController < ApplicationController
+  include ArticleHelper
   
   def index
     @articles = generate_date
@@ -7,52 +8,6 @@ class WelcomeController < ApplicationController
 
 
   private
-
-  def get_front_page_articles(year, month, day)
-    articles = []
-    headline_and_lead_paragraph = {}
-    date = year.to_s+month.to_s+day.to_s
-    nyt_results = HTTParty.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=document_type:article&begin_date=#{date}&end_date=#{date}&page=0&api-key=#{NYTIMES_API_KEY}")
-    nyt_results2 = HTTParty.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=document_type:article&begin_date=#{date}&end_date=#{date}&page=1&api-key=#{NYTIMES_API_KEY}")
-
-    if nyt_results["response"]["docs"].count == 0
-      generate_date
-
-    else
-
-      i = 0
-      while i < nyt_results["response"]["docs"].count
-        nyt_results["response"]["docs"].each do |result|
-          article_info =  Hash["headline", result["headline"], "lead_paragraph", result["lead_paragraph"], "pub_date", result["pub_date"]]
-          articles.push(article_info)
-        end
-        i += 1
-      end
-      i = 0
-      while i < nyt_results2["response"]["docs"].count
-        nyt_results2["response"]["docs"].each do |result|
-          article_info =  Hash["headline", result["headline"], "lead_paragraph", result["lead_paragraph"], "pub_date", result["pub_date"]]
-          articles.push(article_info)
-        end
-        i += 1
-      end
-      articles.uniq!
-    end
-    
-  end
-
-  def generate_date
-    month = Time.new.month
-    if month < 10
-      month = "0" + month.to_s
-    end
-    day = Time.new.day
-    if day < 10
-      day = "0" + day.to_s
-    end
-    year = 1850 + rand(164)
-    get_front_page_articles(year, month, day)
-  end
 
   def load_user
     return @user = User.find(params[:id])
---
+++ app/helpers/article_helper.rb
@@ -0,0 +1,37 @@
+# article_helper.rb
+module ArticleHelper
+
+   def get_front_page_articles(year, month, day)
+    headline_and_lead_paragraph = {}
+    date = year.to_s + month.to_s + day.to_s
+
+    results = [0, 1].map do |page|
+      HTTParty.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=document_type:article&begin_date=#{date}&end_date=#{date}&page=#{page}&api-key=#{NYTIMES_API_KEY}")["response"]["docs"]
+    end.flatten
+
+    if results.count == 0
+      generate_date
+    else
+      articles = results.map do |result|
+        Hash["headline", result["headline"], "lead_paragraph", result["lead_paragraph"], "pub_date", result["pub_date"]]      
+      end
+
+      return articles.uniq
+    end
+  end
+
+  def generate_date
+    month = Time.new.month
+    if month < 10
+      month = "0" + month.to_s
+    end
+    day = Time.new.day
+    if day < 10
+      day = "0" + day.to_s
+    end
+    year = 1850 + rand(164)
+    get_front_page_articles(year, month, day)
+  end
+
+
+end
