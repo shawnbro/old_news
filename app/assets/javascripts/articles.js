45aa11f1aeb5d098e350d910cbc7d7b8854c258e
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-05-04 15:48:02 -0700

    refactored using js

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -3,27 +3,19 @@ var articles;
 var headlineAndLeadParagraph = {};
 var counter = 0;
 
-console.log("hey");
 $( document ).ready( function() {
   getDate();
 })
 
-
-
  function getFrontPageArticles(randomDate){
-
-   console.log(randomDate);
+  d = randomDate;
   var results =  $.getJSON("/articles?date="+randomDate, function(response) {
     articles = response;
-      $("<div class='article'><div class='article_space'><div class='headline'>"+articles[counter]["headline"]["main"]+"</div></div>").appendTo("body");
-
-      $("<div class='lead_paragraph'>"+articles[counter]["lead_paragraph"]+"</div>").appendTo("div.article_space");
-      $("<button id='next'>Next</button>").appendTo("body");
-      $("button#next").on("click", function() {
-        $("div.headline").empty();
-        $("div.lead_paragraph").empty();
-        counter++;
-      })
+    $('h2').text("On this date in "+randomDate[0]+randomDate[1]+randomDate[2]+randomDate[3])
+    for(var i = 0; i < articles.length; i++) {
+      $("<div class='headline' id='"+i+"'>"+articles[i]["headline"]["main"]+"</div>").appendTo(".article-space");
+      $("<div class='lead_paragraph'>"+articles[i]["lead_paragraph"]+"</div>").appendTo("div#"+i+"");
+    }
   })
 
  }
