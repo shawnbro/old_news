750a3e572e9081f73fb70abff5353669e77a802d
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-05-04 16:39:10 -0700

    added hover effect to show paragraphs

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -4,20 +4,34 @@ var headlineAndLeadParagraph = {};
 var counter = 0;
 
 $( document ).ready( function() {
+  //this generates a random date then adds the articles
   getDate();
+  //load new date/article button
+  $('h2#button').click(function() {
+    getDate();
+  })
 })
 
  function getFrontPageArticles(randomDate){
+  $('.article-space').empty();
   d = randomDate;
   var results =  $.getJSON("/articles?date="+randomDate, function(response) {
     articles = response;
     $('h2').text("On this date in "+randomDate[0]+randomDate[1]+randomDate[2]+randomDate[3])
     for(var i = 0; i < articles.length; i++) {
-      $("<div class='headline' id='"+i+"'>"+articles[i].headline.main+"</div>").appendTo(".article-space");
-      // $("<div class='lead_paragraph'>"+articles[i]["lead_paragraph"]+"</div>").appendTo("div#"+i+"");
+      if(articles[i].lead_paragraph !== null) {
+        $("<div class='headline' id='"+i+"'><a href='"+articles[i].web_url+"'>"+articles[i].headline.main+"</div>").appendTo(".article-space");
+        $("div#"+i).mouseenter(function(e) {
+          article = e.target
+          $(article).text(articles[article.id].lead_paragraph);
+        })
+        $("div#"+i).mouseleave(function(e) {
+          article = e.target
+          $(article).text(articles[article.id].headline.main)
+        })
+      }
     }
   })
-
  }
 
   function correctDateSyntax(time) {
